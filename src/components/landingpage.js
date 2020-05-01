ea53aec6d35a40f474d2f6058e5ae3c379312677
Catherine Adegoke <cmadegoke@gmail.com>
2020-05-01 18:17:10 -0400

    projecttab added

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {Grid, Cell} from 'react-mdl';
-import image from './images';
+import image from './images/kate.png'
 
 
 class Landing extends Component {
@@ -14,7 +14,28 @@ class Landing extends Component {
                         className = 'img'/>
                 
             <div className= 'banner-text'>
-                <h1>blahblah</h1>
+                <h1> Catherine Adegoke
+                </h1>
+
+                <hr/>
+                <p>Bootstrap | node.js | express | MongpDB | SQL | React | Javascript | Java</p>
+
+                <div className = 'social-links'>
+                    <a href = 'http://google.com'rel='noopener noreferrer' target = "_blank">
+                        <i className = "fa fa-linkedin-square" aria-hidden ="true"/>
+                        </a>
+
+                        <a href = 'http://google.com'rel='noopener noreferrer' target = "_blank">
+                        <i className = "fa fa-github-square" aria-hidden ="true"/>
+                        </a>
+                        <a href = 'http://google.com'rel='noopener noreferrer' target = "_blank">
+                        <i className = "fa fa-facebook-square" aria-hidden ="true"/>
+                        </a>
+
+
+
+
+                </div>
             </div>
                     </Cell>
                 </Grid>
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,10 +1,66 @@
 import React, {Component} from 'react';
+import {Tabs,Tab,Grid,Cell, Card, CardTitle,CardText,CardActions,CardMenu,IconButton,Button } from 'react-mdl'
 
 class Project extends Component {
+    constructor(props){
+        super(props);
+        this.state={activeTab:0};
+    }
+      categories(){
+    if(this.state.activeTab=== 0){
+        return(
+            <Card shadow={0} style={{width: '450', margin: 'auto'}}>
+    <CardTitle style={{color: '#fff', height: '176px', 
+    background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover'}}>React Project</CardTitle>
+    <CardText>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Mauris sagittis pellentesque lacus eleifend lacinia...
+    </CardText>
+    <CardActions border>
+        <Button colored>Github</Button>
+    </CardActions>
+    <CardMenu style={{color: '#fff'}}>
+        <IconButton name="share" />
+    </CardMenu>
+</Card>
+        )
+    }
+    else if (this.state.activeTab=== 1){
+        return(
+            <div><h1>this is mongo</h1></div>
+        )
+    }
+    else if (this.state.activeTab=== 2){
+        return(
+            <div><h1>this is sql</h1></div>
+        )
+    }
+    else if (this.state.activeTab=== 3){
+        return(
+            <div><h1>this is Javascript</h1></div>
+        )
+    }
+}
     render(){
         return(
-            <div>
-                <h1>Project Page</h1>
+            <div className= "category-tabs">
+                <Tabs activeTab={this.state.activeTab}onChange={(tabId)=>this.setstate({activeTab:tabId})} ripple>
+                    <Tab>React</Tab>
+                    <Tab>MongoDB</Tab>
+                    <Tab>SQL</Tab>
+                    <Tab>Javascript</Tab>
+                </Tabs>
+                <section className ="projects-grid">
+                    <Grid className ="projects-grid">
+                        <Cell col={12}>
+                            <div className = "content">{this.categories()}</div>
+
+
+                        </Cell>
+                    </Grid>
+
+                </section>
+                
             </div>
         )
     }
