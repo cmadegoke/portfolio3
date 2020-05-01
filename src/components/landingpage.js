63da7feb3bfd41ac6b71779d89ca9d3e8f2f0074
Catherine Adegoke <cmadegoke@gmail.com>
2020-05-01 15:29:22 -0400

    adding image

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,12 +1,22 @@
 import React, {Component} from 'react';
 import {Grid, Cell} from 'react-mdl';
+import image from './images';
+
 
 class Landing extends Component {
     render(){
         return(
             <div style = {{width: '100%', margin: 'auto'}}>
                 <Grid className = "landing-grid">
-                    <Cell col ={12}></Cell>
+                    <Cell col ={12}>
+                        <img src= {image}
+                        alt = 'Kate'
+                        className = 'img'/>
+                
+            <div className= 'banner-text'>
+                <h1>blahblah</h1>
+            </div>
+                    </Cell>
                 </Grid>
                 
             </div>
