297ba3a12d487d9d8f33a7f83c4df3ba38afa09a
Catherine Adegoke <cmadegoke@gmail.com>
2020-04-30 23:00:44 -0400

    nav bar done

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import {Layout,Header,Navigation,Drawer,Content} from 'react-mdl';
+import Main from './components/main';
+import {Link} from 'react-router-dom';
+
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="demo-big-content">
+    <Layout>
+        <Header className="header-color" title="Catherine Adegoke" scroll>
+            <Navigation>
+                <Link to="/resume">Resume</Link>
+                <Link to="/aboutme">About Me</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="contact/">Contact Info</Link>
+            </Navigation>
+        </Header>
+        <Drawer title="Title">
+            <Navigation>
+            <Link to="/resume">Resume</Link>
+                <Link to="/aboutme">About Me</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="contact/">Contact Info</Link>
+            </Navigation>
+        </Drawer>
+        <Content>
+            <div className="page-content" />
+            <Main/>
+        </Content>
+    </Layout>
+</div>
+  );
+}
+
+export default App;
