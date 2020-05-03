ba1b9798e46d62abba8f3f185c7b8d34cf49a55b
Catherine Adegoke <cmadegoke@gmail.com>
2020-05-03 17:24:09 -0400

    resume started

---
+++ src/components/eduction.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {Grid,Cell} from 'react-mdl';
+
+
+class Education extends Component {
+    render(){
+        return(
+           <Grid>
+               <Cell></Cell>
+               <Cell></Cell>
+           </Grid>
+        )
+    }
+}
+
+export default Education;
--- src/components/resume.js
+++ src/components/resume.js
@@ -1,10 +1,31 @@
 import React, {Component} from 'react';
+import {Grid,Cell} from "react-mdl";
+import image from './images/kate.png'
+import Education from './eduction'
+
 
 class Resume extends Component {
     render(){
         return(
             <div>
-                <h1>Resume Page</h1>
+                <Grid>
+                    <Cell col={4}>
+                        <div style= {{textAlign:"center"}}>
+                        <img src={image}
+                            alt='Kate'
+                            style={{ height: '250px'}}
+                            className='img' />
+                        </div>
+                        <h2 style = {{paddingTop: "2em"}}>Catherine Adegoke</h2>
+                        
+                        </Cell>
+
+                    <Cell className ='resume' col={8}>
+                    <h2>Education</h2>
+                    
+                    </Cell>
+                </Grid>
+              
             </div>
         )
     }
